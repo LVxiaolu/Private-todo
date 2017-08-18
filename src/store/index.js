480202066c747dc9d2852957c1cafbb51489ce01
LVxiaolu <LVxiaolu8023@163.com>
2017-08-19 01:59:35 +0800

    createStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
-/**
- * Created by fy on 2017/8/19.
- */
+
+import {createStore,combineReducers} from 'redux'
+import {todos,filter} from './reducers'
+//把二个reducer合并成同一个reducer
+let reducer=combineReducers({
+  todos,
+  filter
+});
+
+let store=createStore(reducer);
+
+window.store=store;
+export default store
