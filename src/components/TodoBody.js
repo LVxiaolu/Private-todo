4fd56d1157879d307c1958197dabca5a1785439a
LVxiaolu <LVxiaolu8023@163.com>
2017-08-21 17:55:56 +0800

    TodoBody

--- src/components/TodoBody.js
+++ src/components/TodoBody.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react';
+import * as types from '../store/action-types';
 import {connect} from 'react-redux'
  class TodoBody extends Component {
   render() {
     return (
       <ul className="list-group">
         {
+          this.props.todos.length>0?
+          <li className="list-group-item">
+            <input
+              onChange={event=>this.props.toggleAll(event.target.checked)}
+              type="checkbox"
+              checked={this.props.activeCount==0}/>{this.props.activeCount==0?'全部取消':'全部选中'}
+          </li>:null
+        }
+        {
           this.props.todos.map((item,index)=>(
             <li className="list-group-item" key={index}>
-              <input type="checkbox" checked={item.completed}/>
-              <span style={{marginLeft:10,textDecoration:item.completed?'line-through':''}}>{item.title}</span>
+              <input type="checkbox"
+                     onChange={()=>this.props.toggleTodo(item.id)}
+                     checked={item.completed}/>
+              {
+                this.props.editing===item.id?<input type="text" value={item.title}/>:
+                <span
+                  onDoubleClick={()=>this.props.changeEditing(item.id)}
+                  style={{marginLeft:10,textDecoration:item.completed?'line-through':''}}>{item.title}</span>
+              }
+
               <span className="pull-right">
-                <button className="btn btn-danger btn-xs">X</button>
+                <button
+                  onClick={()=>this.props.delTodo(item.id)}
+                  className="btn btn-danger btn-xs">X</button>
               </span>
             </li>
           ))
@@ -22,7 +42,15 @@ import {connect} from 'react-redux'
 
 export default connect(
   state=>({
-    todos:state.todos.list
-  })
+    todos:state.todos.list,
+    activeCount:state.todos.list.filter(item=>!item.completed).length,
+    editing:state.todos.editing
+  }),
+{
+  delTodo:id=>({type:types.DEL_TODO,id}),
+  toggleTodo:id=>({type:types.TOGGLE_TODO,id}),
+  toggleAll:checked=>({type:types.TOGGLE_ALL,checked}),
+  changeEditing:id=>({type:types.CHANGE_EDITING,id})
+}
 
 )(TodoBody)
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,6 +1,6 @@
 import * as types from '../action-types'
 // 增加 删除 切换完成状态
-export default function (state={list:[]},action) {
+export default function (state={editing:'',list:[]},action) {
   switch (action.type){
     case types.ADD_TODO:
       return{
@@ -12,8 +12,34 @@ export default function (state={list:[]},action) {
         ...state,
         list:state.list.filter(item=>item.id!=action.id)
       };
-
-
+    case types.TOGGLE_TODO:
+      return {
+        ...state,
+        list:state.list.map(item=>{
+          if (item.id==action.id){
+            item.completed=!item.completed;
+          }
+          return item;
+        })
+      };
+    case types.DELETE_ALL_COMPLETED:
+      return {
+        ...state,
+      list:state.list.filter(item=>!item.completed)
+    };
+    case types.TOGGLE_ALL:
+      return {
+        ...state,
+      list:state.list.map(item=>{
+        item.completed=action.checked;
+        return item;
+      })
+      };
+    case types.CHANGE_EDITING:
+      return {
+        ...state,
+        editing:action.id
+      };
     default:
      return state
 
